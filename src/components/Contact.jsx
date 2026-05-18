2feaa0afdb08852ed8cb89aa1c50817421f75c89
Aamin-Mansuri <aaminmansuri988@gmail.com>
2026-05-18 07:41:42 -0700

    maintainence

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,14 +19,14 @@ const Contact = () => {
 
            <div id='footer' className='pt-35 flex flex-col items-center gap-20 '>
               <div className='font-black text-8xl text-center font-sans'>
-                <h1 id='f=h1' data-aos="fade-up">INTERESTED IN WORKING <br />TOGETHER?</h1>
+                <h1 id='f-h1' data-aos="fade-up">INTERESTED IN WORKING <br />TOGETHER?</h1>
                </div>
               <div className='text-center'>
                 <p data-aos="fade-up" className='font-medium font-sans text-2xl'>Drop me an email:</p>
                 <h1 id='f-mail' data-aos="fade-up"><a id='f-mail' className='font-bold font-sans text-3xl' href="mailto:aaminmansuri988@gmail.com">aaminmansuri988@gmail.com</a></h1>
               </div>
 
-              <div id='l-foot' className='flex justify-center gap-110  p-2 pb-8 pt-6 border-t-2 '>
+              <div id='l-foot' className='flex justify-between gap-0 w-screen p-2 pb-8 pt-6 pl-8 pr-8 border-t-2 '>
                 <div id='copy' className='font-medium font-sans text-2xs'>Copyright © 2023 Unkown</div>
                 <div id='f-bt' className='flex gap-10 items-center pr-18'>
                     <button id='i-bt' className=' rounded-3xl  bg-white w-40 py-3 border-2 hover:bg-blue-500 font-medium  text-black text-2xs'><a href="https://github.com/Aamin-Mansuri">GitHub ⬈</a></button> 
